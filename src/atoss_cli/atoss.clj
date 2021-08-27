3b2d6113277191a6ab4b26ab78ececc5641783e3
Daniils Petrovs <daniils@platogo.com>
2021-08-27 17:19:09 +0200

    Implement navigation to time correction form

--- src/atoss_cli/atoss.clj
+++ src/atoss_cli/atoss.clj
@@ -1,9 +1,12 @@
 (ns atoss-cli.atoss
   (:require [etaoin.api :as api]
-            [etaoin.keys :as keys]))
+            [etaoin.keys :as keys])
+  (:gen-class))
 
 (def atoss-url "https://ases.novomatic.com/SES/html")
 (def atoss-creds {:username "" :password ""})
+
+(def fav-btn {:css ".is-favorite"})
 
 (defn login
   "Perform login into ATOSS dashboard."
@@ -16,3 +19,12 @@
     (api/fill-active driver keys/tab)
     (api/fill-active driver (atoss-creds :password))
     (api/fill-active driver keys/enter)))
+
+(defn nav-to-time-correction
+  "Navigate the driver to the time correction page.
+  This is where all of the data entry must happen."
+  [driver]
+  (doto driver
+    (api/switch-frame-parent)
+    (api/click fav-btn)
+    (api/wait-visible driver {:tag :span :fn/has-text "Tagescode"})))
--- src/atoss_cli/core.clj
+++ src/atoss_cli/core.clj
@@ -1,6 +1,5 @@
 (ns atoss-cli.core
-  (:require [etaoin.api :refer :all])
   (:gen-class))
 
 (defn -main [& args]
-  (print "hello world"))
+  (println "Hello world"))
