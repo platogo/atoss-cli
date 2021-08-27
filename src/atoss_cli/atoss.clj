0e015b24b10b74f884976b45af777b9e5e9356d6
Daniils Petrovs <daniils@platogo.com>
2021-08-27 19:04:10 +0200

    Fix issue with redundant passing of driver symbol

--- src/atoss_cli/atoss.clj
+++ src/atoss_cli/atoss.clj
@@ -4,21 +4,22 @@
   (:gen-class))
 
 (def atoss-url "https://ases.novomatic.com/SES/html")
-(def atoss-creds {:username "" :password ""})
+(def workday {:def-start-time "9:00"
+              :def-end-time "17:30"})
 
 (def fav-btn {:css ".is-favorite"})
 
 (defn login
-  "Perform login into ATOSS dashboard."
-  [driver]
-  (println "Logging into ATOSS")
+  "Perform login into ATOSS dashboard using provided ATOSS credentials."
+  [driver {user :username pass :password}]
+  (println "Logging into ATOSS with user: " user)
   (doto driver
     (api/go atoss-url)
-    (api/wait 5) ;; TODO: replace with proper waiter
-    (api/fill-active driver (atoss-creds :username))
-    (api/fill-active driver keys/tab)
-    (api/fill-active driver (atoss-creds :password))
-    (api/fill-active driver keys/enter)))
+    (api/wait 3) ;; TODO: replace with proper waiter
+    (api/fill-active user)
+    (api/fill-active keys/tab)
+    (api/fill-active pass)
+    (api/fill-active keys/enter)))
 
 (defn nav-to-time-correction
   "Navigate the driver to the time correction page.
@@ -27,4 +28,4 @@
   (doto driver
     (api/switch-frame-parent)
     (api/click fav-btn)
-    (api/wait-visible driver {:tag :span :fn/has-text "Tagescode"})))
+    (api/wait-visible {:tag :span :fn/has-text "Tagescode"})))
--- src/atoss_cli/core.clj
+++ src/atoss_cli/core.clj
@@ -1,5 +1,19 @@
 (ns atoss-cli.core
+  (:require [clojure.string :as str]
+            )
   (:gen-class))
 
+;; (def atoss-creds {:username (env :atoss_username)
+;;                   :password (env :atoss_password)})
+
+(def desc "ATOSS CLI by Platogo Gmbh.")
+
 (defn -main [& args]
-  (println "Hello world"))
+  (println desc)
+  (if (= (count args) 2)
+    (do
+      (doseq [arg args]
+        (println arg))
+      ;; create a driver and pass to atos
+      )
+    ))
