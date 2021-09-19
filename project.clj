2ff9197c5b85e6c8f29502a5612c21eab24f6fa3
Daniils Petrovs <daniils@platogo.com>
2021-09-19 23:23:40 +0200

    Performance optimizations and GraalVM experiments

--- project.clj
+++ project.clj
@@ -8,10 +8,27 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [etaoin "0.4.5"]
                  [org.clojure/tools.cli "1.0.206"]]
+  :native-image {:name "atoss-native"
+                 :opts ["--report-unsupported-elements-at-runtime" ;; ignore native-image build errors
+                        "-H:+ReportExceptionStackTraces"
+                        "--allow-incomplete-classpath"
+                        "--no-server"
+                        "--no-fallback"
+                        "--initialize-at-run-time=org.apache.http.impl.auth.NTLMEngineImpl"
+                        "--initialize-at-run-time=clojure.core.server,clojure.lang"
+                        "--initialize-at-build-time=clojure.spec.gen.alpha,clojure,clojure.core.server"
+                        "--enable-url-protocols=http,https"
+                        ]}
   :target-path "target/%s"
   :jar-name "atoss-cli.jar"
   :uberjar-name "atoss-cli-standalone.jar"
 
-  :profiles {:uberjar {:aot :all}}
-  :plugins [[lein-cljfmt "0.8.0"]]
+  :global-vars {*warn-on-reflection* true}
+
+  :profiles {:uberjar {:aot :all
+                       :native-image {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
+             }
+  :plugins [[lein-cljfmt "0.8.0"]
+            [io.taylorwood/lein-native-image "0.3.1"]]
+  :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
   :repl-options {:init-ns atoss-cli.core})
