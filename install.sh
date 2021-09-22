90b55a72f9336580d61b52a77799195307286e94
Daniils Petrovs <daniils@platogo.com>
2021-09-22 12:55:46 +0200

    fixup! Update install script jar url

--- install.sh
+++ install.sh
@@ -10,14 +10,14 @@ LOCAL_JAR_FILE=$PWD/target/uberjar/atoss-cli-standalone.jar
 
 if [ -f "$LOCAL_JAR_FILE" ]; then
     echo "Local uberjar exists, skipping pull from remote."
-	cp -fr $PWD/target/uberjar/atoss-cli-standalone.jar /usr/local/bin/
+	cp -f $PWD/target/uberjar/atoss-cli-standalone.jar /usr/local/bin/
 else
     echo "$LOCAL_JAR_FILE does not exist, pulling release from Github."
 	curl -L $LATEST_REL_URL > atoss-cli-standalone.jar
-	mv -fr atoss-cli-standalone.jar /usr/local/bin/
+	mv -f atoss-cli-standalone.jar /usr/local/bin/
 fi
 
 echo "Installing wrapper"
 
-cp -fr $PWD/wrapper.sh "/usr/local/bin/atoss-cli"
+cp -f $PWD/wrapper.sh "/usr/local/bin/atoss-cli"
 
