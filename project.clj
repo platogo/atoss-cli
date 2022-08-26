81991e9375d6267e4cfe8d39974a16ee412eb2ea
Daniils Petrovs <daniils@platogo.com>
2022-08-26 16:40:59 +0200

    Bump project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject atoss-cli "0.3.2-SNAPSHOT"
+(defproject atoss-cli "0.3.3-SNAPSHOT"
   :description "A CLI tool for interacting with ATOSS time sheets"
   :url "https://github.com/platogo/atoss-cli"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
