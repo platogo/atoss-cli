e155ec68c454adfad73379307aa210f036f6ed1e
Peter Krenn <peter.krenn@gmail.com>
2025-12-23 21:52:45 +0100

    Remove outdated clojure version

--- shell.nix
+++
@@ -1,4 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    nativeBuildInputs = with pkgs.buildPackages; [ clojure leiningen chromedriver ];
-}
--- test/atoss_cli/core_test.clj
+++
@@ -1,6 +0,0 @@
-(ns atoss-cli.core-test
-  (:require [clojure.test :refer [deftest testing is]]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
